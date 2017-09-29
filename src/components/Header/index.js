b68585c60f7d9cd050dd80b303379398a183cbe7
Lody Borgers <l.borgers@taf.nl>
2017-09-29 14:23:28 +0200

    Created Header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <header className="Header">
+        <h1 className="Header-Title">The Q</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
