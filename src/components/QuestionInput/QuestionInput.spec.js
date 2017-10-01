e4334b2abafd8e3758f1eb5e6a02c16234cd7d72
Lody Borgers <hi@lodybo.nl>
2017-10-01 19:04:34 +0200

    Worked on testing with React and Enzyme

--- src/components/QuestionInput/QuestionInput.spec.js
+++ src/components/QuestionInput/QuestionInput.spec.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { mount, shallow } from 'enzyme';
+
 import QuestionInput from './index';
 
 describe('The QuestionInput component', () => {
   it('should render', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<QuestionInput />, div);
+    shallow(<QuestionInput />);
+  });
+
+  it('should fetch the entered data', () => {
+    const component = mount(<QuestionInput />);
+
+    const textField = component.find({ name: 'question' });
+    const textArea = component.find('textarea');
+
+    textField.simulate('change', { target: { value: 'Who is the biggest tire manufacturer in the world?' } });
+    // textArea.simulate('change', { target: { value: 'Lego, they even were in the Guiness Book of World Records with it'}});
+
+    expect(textField).toBeDefined();
+    expect(textArea).toBeDefined();
+
+    expect(textField.get(0).props.value).toBe('Who is the biggest tire manufacturer in the world?');
   });
 });
